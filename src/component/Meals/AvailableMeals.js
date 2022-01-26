4470cf165244a9b2c939235e264b2636fd25177c
Erik-NA7 <p.erikson.saragi@gmail.com>
2022-01-27 00:28:32 +0700

    checkout form

--- src/component/Meals/AvailableMeals.js
+++ src/component/Meals/AvailableMeals.js
@@ -1,10 +1,66 @@
-import { DUMMY_MEALS } from "./dummy-meals";
+import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map( meal =>
+  const [ mealsData, setMealsData ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [isError, setIsError] = useState()
+
+  useEffect(() => {
+    const getMeals = async () => {
+      setIsLoading(true)
+      const response = await fetch('https://learnfirebase-ceee0-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+      const json = await response.json()
+      const loadedMeals = []
+      
+      for (const key in json) {
+        loadedMeals.push({
+          id: key,
+          name: json[key].name,
+          description: json[key].description,
+          price: json[key].price
+        }) 
+      }
+      
+      setMealsData(loadedMeals)
+      setIsLoading(false)     
+    }
+
+
+    getMeals().catch(error => {
+      setIsLoading(false)
+      setIsError(error.message)
+    })
+    
+    return () => {
+      getMeals()
+    }
+  }, [])
+  
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (isError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{isError}</p>
+      </section>   
+    )
+  }
+
+  const mealsList = mealsData.map( meal =>
+    
     <MealItem
       id={meal.id}
       key={meal.id}
